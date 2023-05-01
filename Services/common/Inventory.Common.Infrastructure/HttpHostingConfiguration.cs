be5d468f31f261c4429253537e4cf2cd26ac6e50
ygo74 <yannickgobert@yahoo.fr>
2023-05-01 08:18:37 +0200

    Create HttpHostingConfiguration.cs

---
+++ Services/common/Inventory.Common.Infrastructure/HttpHostingConfiguration.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Common.Infrastructure.Http.Configuration
+{
+    public class HttpHostingConfiguration
+    {
+        public const string ConfigurationSection = "HttpHostingConfiguration";
+
+        public bool? UseReverseProxy { get; set; }
+
+        public string? ProxyBasePath { get; set; }
+
+        public string? ProxyScheme { get; set; }
+
+        public bool? EnableHttpHeadersLogging { get; set; }
+
+    }
+}
